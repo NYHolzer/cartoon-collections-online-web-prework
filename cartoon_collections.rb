18eaa9b392af25de292f5d61fa45001b0afb16cb
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-13 01:51:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,9 +22,9 @@ def long_planeteer_calls (array)
   end
 end
 
-def find_the_cheese (food)
+def find_the_cheese (type)
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find do |x|
-    x.include? (cheese_types)
+    cheese_types.include? (type)
   end
 end
